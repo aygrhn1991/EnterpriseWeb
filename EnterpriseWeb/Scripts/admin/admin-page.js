0432cdbe78fe359916d180f093e97bbdcc07db63
chenyufeng <411610370@qq.com>
2018-01-23 21:48:33 +0800

    1

--- EnterpriseWeb/Scripts/admin/admin-page.js
+++ EnterpriseWeb/Scripts/admin/admin-page.js
@@ -1,4 +1,5 @@
 ﻿app.controller('pageList', function ($scope, $http) {});
 app.controller('pageAdd', function ($scope, $http) {
     var ue = UE.getEditor('container');
+    console.log(ue.getOpt('scaleEnabled'));
 });
